167731c0398be4bb41a198de805b7e7af2b08351
Khushichauhan <kc6478576@gmail.com>
2024-09-17 17:45:21 +0530

    First Commit

---
+++ Backend/server.js
@@ -0,0 +1,46 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('server is ready');
+});
+
+//get a list of five jokes
+
+app.get('/api/jokes', (req, res) => {    //(/api) is a standard practice
+    const jokes = [
+        {
+            id: 1,
+            title: 'A joke',
+            content: 'this is a joke'
+        },
+        {
+            id: 1,
+            title: 'Another joke',
+            content: 'this is another joke'
+        },
+        {
+            id: 1,
+            title: 'A third joke',
+            content: 'this is a third joke'
+        },
+        {
+            id: 1,
+            title: 'A fourth joke',
+            content: 'this is a fourth joke'
+        },
+        {
+            id: 1,
+            title: 'A fifth joke',
+            content: 'this is a fifth joke'
+        },
+    ];
+    res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`server at http://localhost:${port}`);
+});
